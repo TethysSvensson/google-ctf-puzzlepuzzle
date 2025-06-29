2d7fb68eb5ac9a169c84dd135fecf38075e006aa
Tethys Svensson <freaken@freaken.dk>
2025-06-29 20:03:39 +0200

    FLAG

--- src/bin/cat_flag.rs
+++ src/bin/cat_flag.rs
@@ -1,13 +1,30 @@
+use core::panic;
+
 fn main() {
     let map = tools::read_map();
-    for x in (8426..17235).step_by(24).rev() {
+    let mut bits = Vec::new();
+    for x in (8427..17236).step_by(24).rev() {
         if map[5][x] == tools::ACTIVE {
-            print!("1");
+            bits.push(true)
         } else if map[5][x] == tools::NOT_ACTIVE {
-            print!("0");
+            bits.push(false);
         } else {
-            print!("?");
+            panic!("Unexpected tile value at (5, {}): {}", x, map[5][x]);
         }
     }
-    println!()
+    assert!(
+        bits.len() % 8 == 0,
+        "Bit length is not a multiple of 8: {}",
+        bits.len()
+    );
+    print!("CTF{{");
+    for chunk in bits.chunks(8).rev() {
+        let byte = chunk
+            .iter()
+            .rev()
+            .enumerate()
+            .fold(0u8, |acc, (i, &bit)| acc | ((bit as u8) << (7 - i)));
+        print!("{}", byte as char);
+    }
+    println!("}}");
 }
