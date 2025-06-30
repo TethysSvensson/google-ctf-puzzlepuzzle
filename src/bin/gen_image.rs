89e9685715f25a701d504aa6807f3780f52b3afd
Tethys Svensson <freaken@freaken.dk>
2025-06-30 15:28:45 +0200

    Better image compression

--- src/bin/gen_image.rs
+++ src/bin/gen_image.rs
@@ -1,47 +1,62 @@
 use clap::Parser;
-use image::{ImageBuffer, ImageFormat, Rgb};
+use image::{ImageEncoder, codecs::png::PngEncoder};
 use std::fs::File;
 use std::io::BufWriter;
 use tools::{H, W, read_map};
 
 #[derive(clap::Parser)]
 struct Args {
-    x1: u32,
-    x2: u32,
-    y1: u32,
-    y2: u32,
+    x1: usize,
+    x2: usize,
+    y1: usize,
+    y2: usize,
 }
 
 fn main() {
     let args = Args::parse();
     let map = read_map();
 
-    let width = (args.x2 - args.x1).min(W as u32 - args.x1);
-    let height = (args.y2 - args.y1).min(H as u32 - args.y1);
+    let width = (args.x2 - args.x1).min(W - args.x1);
+    let height = (args.y2 - args.y1).min(H - args.y1);
 
-    let mut img = ImageBuffer::<Rgb<u8>, _>::new(width, height);
+    let mut img = vec![0; width * height * 3];
 
-    for (x, y, pixel) in img.enumerate_pixels_mut() {
-        *pixel = match map[(args.y1 + y) as usize][(args.x1 + x) as usize] {
-            0 => Rgb([0, 0, 0]),
-            1 => Rgb([255, 0, 0]),
-            2 => Rgb([255, 255, 0]),
-            3 => Rgb([0, 255, 0]),
-            5 => Rgb([255, 255, 255]),
-            7 => Rgb([128, 128, 128]),
-            8 => Rgb([231, 141, 14]),
-            10 => Rgb([255, 0, 255]),
-            _ => Rgb([0, 0, 255]),
-            // n => todo!("{n}"),
+    for y in 0..height {
+        for x in 0..width {
+            let pixel = match map[(args.y1 + y) as usize][(args.x1 + x) as usize] {
+                0 => [0, 0, 0],
+                1 => [255, 0, 0],
+                2 => [255, 255, 0],
+                3 => [0, 255, 0],
+                5 => [255, 255, 255],
+                7 => [128, 128, 128],
+                8 => [231, 141, 14],
+                10 => [255, 0, 255],
+                _ => [0, 0, 255],
+                // n => todo!("{n}"),
+            };
+            img[(y * width + x) * 3 + 0] = pixel[0];
+            img[(y * width + x) * 3 + 1] = pixel[1];
+            img[(y * width + x) * 3 + 2] = pixel[2];
         }
     }
 
     // Save the image to a file
     let path = "output_image.png";
     let file = File::create(path).expect("Failed to create image file");
-    let ref mut writer = BufWriter::new(file);
+    let writer = PngEncoder::new_with_quality(
+        BufWriter::new(file),
+        image::codecs::png::CompressionType::Best,
+        image::codecs::png::FilterType::NoFilter,
+    );
 
-    img.write_to(writer, ImageFormat::Png)
+    writer
+        .write_image(
+            &img,
+            width as u32,
+            height as u32,
+            image::ExtendedColorType::Rgb8,
+        )
         .expect("Failed to save image");
 
     println!("Image saved to {}", path);
